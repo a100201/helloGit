6e96e56659bb3d36f51883a1305f523181bf72da
looper <lanjian@lanjiandeMacBook-Pro.local>
2017-06-28 15:41:32 +0800

    first commit

---
+++ Desktop/Git/helloGit/testGit/testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by looper on 17/6/28.
+//  Copyright © 2017年 cloutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
