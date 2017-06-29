d02d7b69f8fca0acf61df8d27788125d2a325203
loop <1002015378@qq.com>
2017-06-29 11:29:25 +0800

    sixth

---
+++ testGit/testGit/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  testGit
+//
+//  Created by looper on 17/6/28.
+//  Copyright © 2017年 cloutech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"hello.git");
+    NSLog(@"branchA");
+    NSLog(@"tag2.0");
+    NSLog(@"fourth push");
+    NSLog(@"commit for fifth");
+    self.view.backgroundColor = [UIColor redColor];
+    NSLog(@"sixth");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
